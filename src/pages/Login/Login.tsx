634bd4f61528e338a03f1ea468c914db53814739
nick6850 <137728530+nick6850@users.noreply.github.com>
2024-02-15 14:30:52 +0300

    Refactored login page and added styles

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,26 +1,79 @@
-import React from "react";
-import { useNavigate } from "react-router";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./Login.module.scss";
+
+interface Credentials {
+  email: string;
+  password: string;
+}
 
 function Login() {
+  const [credentials, setCredentials] = useState<Credentials>({
+    email: "",
+    password: "",
+  });
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [error, setError] = useState("");
+
   const navigate = useNavigate();
 
-  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    const { name, value } = event.target;
+    setCredentials((prevCredentials) => ({
+      ...prevCredentials,
+      [name]: value,
+    }));
+  };
+
+  const handleLogin = (event: React.FormEvent<HTMLFormElement>): void => {
+    setError("");
     event.preventDefault();
+    if (
+      credentials.email === "admin@friendnet.com" &&
+      credentials.password === "admin"
+    ) {
+      setIsLoggedIn(true);
+    } else {
+      setError("Неверный логин или пароль");
+    }
+  };
+
+  const handleLogout = (): void => {
+    setIsLoggedIn(false);
+    setCredentials({ email: "", password: "" });
+  };
+
+  if (isLoggedIn) {
     navigate("/home");
+    return null;
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h1>Login page</h1>
-      <label htmlFor="email">Your login</label>
-      <input id="email" type="email" placeholder="Type in your email..." />
-      <label htmlFor="password">Your password:</label>
-      <input
-        id="password"
-        type="password"
-        placeholder="Type in your password..."
-      />
-      <button type="submit">Log in</button>
+    <form className={styles.container} onSubmit={handleLogin}>
+      <label>
+        Ваш email:
+        <input
+          type="email"
+          name="email"
+          value={credentials.email}
+          onChange={handleInputChange}
+        />
+      </label>
+      <br />
+      <label>
+        Пароль:
+        <input
+          type="password"
+          name="password"
+          value={credentials.password}
+          onChange={handleInputChange}
+        />
+      </label>
+      <br />
+      <button type="submit">Войти</button>
+      {error && <div className={styles.error}>{error}</div>}
     </form>
   );
 }
